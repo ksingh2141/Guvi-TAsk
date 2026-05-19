8a089a56eeac89edffe41ad415168278c28912da
ksingh2141 <kundansingh2141@gmail.com>
2026-05-19 19:38:14 +0530

    Add files via upload

---
+++ Task 14/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture()
+def setup():
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install())
+    )
+
+    driver.maximize_window()
+
+    driver.get("https://www.zenclass.in/login")
+
+    yield driver
+
+    driver.quit()
+
